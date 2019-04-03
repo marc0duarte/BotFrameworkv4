51daaa7d67fb67adf89d5aab81f62d931eb842e9
Sebastián Alvarado <salvarado.adessa@gmail.com>
2019-04-03 12:39:00 -0300

    Se agrega descargar archivos

--- bot.js
+++ bot.js
@@ -5,17 +5,17 @@
 const { LuisRecognizer } = require('botbuilder-ai');
 const { ActionTypes, ActivityTypes, CardFactory, AttachmentLayoutTypes } = require('botbuilder');
 
-var Memoria = function(_id,_url){
+var Memoria = function (_id, _url) {
     this.id = _id,
-    this.url = _url
-    }
+        this.url = _url
+}
 
 var Memorias = [];
-Memorias.push(new Memoria(2017,'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/MemoriaFalabella2017_SVS_(finalwebIR)-(1).pdf'));
-Memorias.push(new Memoria(2016,'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/MemoriaSVS-2016.pdf'));
-Memorias.push(new Memoria(2015,'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/MemoriaFalabellaSVS.pdf'));
-Memorias.push(new Memoria(2014,'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/MemoriaEnviadaSVS_20140424.pdf'));
-Memorias.push(new Memoria(2013,'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/2013-Memoria-SVS.pdf'));
+Memorias.push(new Memoria(2017, 'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/MemoriaFalabella2017_SVS_(finalwebIR)-(1).pdf'));
+Memorias.push(new Memoria(2016, 'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/MemoriaSVS-2016.pdf'));
+Memorias.push(new Memoria(2015, 'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/MemoriaFalabellaSVS.pdf'));
+Memorias.push(new Memoria(2014, 'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/MemoriaEnviadaSVS_20140424.pdf'));
+Memorias.push(new Memoria(2013, 'https://s22.q4cdn.com/351912490/files/doc_financials/annual_spanish/2013-Memoria-SVS.pdf'));
 /**
  * A simple bot that responds to utterances with answers from the Language Understanding (LUIS) service.
  * If an answer is not found for an utterance, the bot responds with help.
@@ -37,36 +37,42 @@ class LuisBot {
      */
     async onTurn(turnContext) {
 
-        
+
         // By checking the incoming Activity type, the bot only calls LUIS in appropriate cases.
         if (turnContext.activity.type === ActivityTypes.Message) {
             // Perform a call to LUIS to retrieve results for the user's message.
             const results = await this.luisRecognizer.recognize(turnContext);
             const input = turnContext.activity.text;
-            console.log('Texto enviado ',input)
+            console.log('Texto enviado ', input)
             // Since the LuisRecognizer was configured to include the raw results, get the `topScoringIntent` as specified by LUIS.
             const topIntent = results.luisResult.topScoringIntent;
-            if(topIntent.intent === 'Saludo'){
+            if (topIntent.intent === 'Saludo') {
                 await LuisBot.EnviarSaludos(turnContext)
             }
-            else if (topIntent.intent === 'Memoria'){
+            else if (topIntent.intent === 'Memoria') {
                 await LuisBot.MostrarCarruselMemorias(turnContext);
             }
-            else if (topIntent.intent === 'Video'){
+            else if (topIntent.intent === 'Video') {
                 await LuisBot.GenerarVideoCard(turnContext);
             }
-            else if(topIntent.intent === 'ListaContacto'){
+            else if (topIntent.intent === 'ListaContacto') {
                 await LuisBot.CarruselListaContactos(turnContext);
             }
-            else if(topIntent.intent === 'Opciones'){
+            else if (topIntent.intent === 'Opciones') {
                 await LuisBot.MenuOpciones(turnContext);
+            }
+            else if (topIntent.intent === 'Descargar') {
+                await LuisBot.filesAttachment(turnContext)
+            }
+            else if (topIntent.intent === 'Adjuntar') {
+                await LuisBot.AdjuntarArchivo(turnContext)
             }
             else {
                 // If the top scoring intent was "None" tell the user no valid intents were found and provide help.
                 await turnContext.sendActivity(`No pude entender lo que me preguntas... 😰😰😰
                                                 \n 
                                                 \nTen paciencia... me estan enseñando cosas nuevas todos los días para poder entenderte mejor.😉😉`);
-            
+
                 await LuisBot.AyudarenAlgoMas(turnContext);
             }
         } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate &&
@@ -75,28 +81,28 @@ class LuisBot {
             await turnContext.sendActivity('Bienvenido al Asistente Virtual. 👨‍💻 Realiza las preguntas que tengas y yo te daré la respuesta 👍');
         } else if (turnContext.activity.type !== ActivityTypes.ConversationUpdate) {
             // Respond to all other Activity types.
-            await turnContext.sendActivity(`[${ turnContext.activity.type }]-type activity detected.`);
+            await turnContext.sendActivity(`[${turnContext.activity.type}]-type activity detected.`);
         }
     }
 
-    static async GenerarVideoCard(turnContext){
+    static async GenerarVideoCard(turnContext) {
 
         await turnContext.sendActivities(
             [
-                {type: 'typing'},
-                {type: 'delay', value: 2000},
-                {type: 'message', text: 'Por el momento solo tengo información de un video... Estoy recopilando más videos para poder presentarte!!!'},
-                {type: 'delay', value: 3000},
-                {type: 'message', text: 'Te voy a presentar información sobre un campeonato de proyectos innovadores... Te va a encantar ❤️'},
-                {type: 'typing'},
-                {type: 'delay', value: 4000},
+                { type: 'typing' },
+                { type: 'delay', value: 2000 },
+                { type: 'message', text: 'Por el momento solo tengo información de un video... Estoy recopilando más videos para poder presentarte!!!' },
+                { type: 'delay', value: 3000 },
+                { type: 'message', text: 'Te voy a presentar información sobre un campeonato de proyectos innovadores... Te va a encantar ❤️' },
+                { type: 'typing' },
+                { type: 'delay', value: 4000 },
 
             ]
         )
 
         const video = CardFactory.videoCard(
-            'Campeonato Mundial de IC18 World Championship', 
-            [{url: 'https://sec.ch9.ms/ch9/783d/d57287a5-185f-4df9-aa08-fcab699a783d/IC18WorldChampionshipIntro2.mp4'}],
+            'Campeonato Mundial de IC18 World Championship',
+            [{ url: 'https://sec.ch9.ms/ch9/783d/d57287a5-185f-4df9-aa08-fcab699a783d/IC18WorldChampionshipIntro2.mp4' }],
             [{
                 type: 'openUrl',
                 title: 'Conocer más',
@@ -106,8 +112,8 @@ class LuisBot {
                 subtitle: 'by Microsoft',
                 text: 'Microsoft\'s Imagine Cup es un evento para estudiantes innovadores alrededor del mundo, con el objetivo de crear proyectos innovadores y que tragan beneficio a la comunidad.',
             }
-            )
-            
+        )
+
 
         await turnContext.sendActivity(
             {
@@ -124,40 +130,40 @@ class LuisBot {
     static async EnviarSaludos(turnContext) {
 
         await turnContext.sendActivities([
-            {type: 'typing'},
-            {type: 'delay', value: 2000},
-            {type: 'message', text: 'Gracias por saludar!!! 🥰' }
+            { type: 'typing' },
+            { type: 'delay', value: 2000 },
+            { type: 'message', text: 'Gracias por saludar!!! 🥰' }
         ]);
 
         await turnContext.sendActivities([
-            {type: 'typing'},
-            {type: 'delay', value: 2000},
-            {type: 'message', text: 'Si bien me crearon como un piloto, ya se muchas cosas las cuales podria presentarte 😎' }
+            { type: 'typing' },
+            { type: 'delay', value: 2000 },
+            { type: 'message', text: 'Si bien me crearon como un piloto, ya se muchas cosas las cuales podria presentarte 😎' }
         ]);
 
         await turnContext.sendActivities([
-            {type: 'typing'},
-            {type: 'delay', value: 2000},
-            {type: 'message', text: 'Estoy preparando la información... 🤯' },
-            {type: 'typing'},
-            {type: 'delay', value: 1000},
-            {type: 'message', text: '👨‍🎓👨‍🎓' }
+            { type: 'typing' },
+            { type: 'delay', value: 2000 },
+            { type: 'message', text: 'Estoy preparando la información... 🤯' },
+            { type: 'typing' },
+            { type: 'delay', value: 1000 },
+            { type: 'message', text: '👨‍🎓👨‍🎓' }
         ]);
 
         await LuisBot.MenuOpciones(turnContext);
     }
 
-    static async CarruselListaContactos(turnContext){
+    static async CarruselListaContactos(turnContext) {
 
 
         await turnContext.sendActivity({
             text: 'Estos son los miembros de la empresa:',
             attachments: [LuisBot.CrearTarjetaListaContacto(),
-                          LuisBot.CrearTarjetaListaContacto(),
-                          LuisBot.CrearTarjetaListaContacto(),
-                          LuisBot.CrearTarjetaListaContacto(),
-                          LuisBot.CrearTarjetaListaContacto()                         
-         ],
+            LuisBot.CrearTarjetaListaContacto(),
+            LuisBot.CrearTarjetaListaContacto(),
+            LuisBot.CrearTarjetaListaContacto(),
+            LuisBot.CrearTarjetaListaContacto()
+            ],
             attachmentLayout: AttachmentLayoutTypes.Carousel
 
         });
@@ -166,7 +172,7 @@ class LuisBot {
 
     }
 
-    static CrearTarjetaListaContacto(){
+    static CrearTarjetaListaContacto() {
         return CardFactory.adaptiveCard({
             "type": "AdaptiveCard",
             "body": [
@@ -240,36 +246,36 @@ class LuisBot {
             "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
             "version": "1.0"
         }
-        ) 
+        )
 
     }
 
-    static async MostrarCarruselMemorias(turnContext){
+    static async MostrarCarruselMemorias(turnContext) {
 
         await turnContext.sendActivities([
-            {type: 'typing'},
-            {type: 'delay', value: 2000},
-            {type: 'message', text: '🔍🔎 🔍🔎 Deja buscar a ver si encuentro algo en mi biblioteca de registros... 🔍🔎 🔍🔎'},
-            {type: 'typing'},
-            {type: 'delay', value: 3000}
+            { type: 'typing' },
+            { type: 'delay', value: 2000 },
+            { type: 'message', text: '🔍🔎 🔍🔎 Deja buscar a ver si encuentro algo en mi biblioteca de registros... 🔍🔎 🔍🔎' },
+            { type: 'typing' },
+            { type: 'delay', value: 3000 }
         ])
 
         await turnContext.sendActivities([
-            {type: 'typing'},
-            {type: 'delay', value: 1500},
-            {type: 'message', text: 'Buscando en mis registros... He encontrado los siguientes reportes anuales...  📄 📑 📊'},
-            {type: 'typing'},
-            {type: 'delay', value: 3000}
+            { type: 'typing' },
+            { type: 'delay', value: 1500 },
+            { type: 'message', text: 'Buscando en mis registros... He encontrado los siguientes reportes anuales...  📄 📑 📊' },
+            { type: 'typing' },
+            { type: 'delay', value: 3000 }
         ])
 
         await turnContext.sendActivity({
             text: 'Selecciona un año:',
-            attachments: [LuisBot.MemoriaCard(Memorias[0].id,Memorias[0].url),
-                          LuisBot.MemoriaCard(Memorias[1].id,Memorias[1].url),
-                          LuisBot.MemoriaCard(Memorias[2].id,Memorias[2].url),
-                          LuisBot.MemoriaCard(Memorias[3].id,Memorias[3].url),
-                          LuisBot.MemoriaCard(Memorias[4].id,Memorias[4].url)                         
-         ],
+            attachments: [LuisBot.MemoriaCard(Memorias[0].id, Memorias[0].url),
+            LuisBot.MemoriaCard(Memorias[1].id, Memorias[1].url),
+            LuisBot.MemoriaCard(Memorias[2].id, Memorias[2].url),
+            LuisBot.MemoriaCard(Memorias[3].id, Memorias[3].url),
+            LuisBot.MemoriaCard(Memorias[4].id, Memorias[4].url)
+            ],
             attachmentLayout: AttachmentLayoutTypes.Carousel
 
         });
@@ -277,10 +283,10 @@ class LuisBot {
         await LuisBot.AyudarenAlgoMas(turnContext)
     }
 
-    static MemoriaCard(id,url) {
+    static MemoriaCard(id, url) {
         return CardFactory.heroCard(
-            id+' Reporte Anual',
-            CardFactory.images([__dirname +'/resources/img/'+'MemoriaFalabella'+id+'.png']),
+            id + ' Reporte Anual',
+            CardFactory.images([__dirname + '/resources/img/' + 'MemoriaFalabella' + id + '.png']),
             CardFactory.actions([
                 {
                     type: 'openUrl',
@@ -288,29 +294,90 @@ class LuisBot {
                     value: url
                 }
             ])
-    
+
         );
     }
 
-    static async AyudarenAlgoMas(turnContext){
+    static async filesAttachment(turnContext) {
+        let pdffile = __dirname + '/resources/files/pdf/Q1.pdf'
+        let pdf = {
+            contentType: 'application/vnd.microsoft.card.hero',
+            content: {
+                buttons: [{
+                    title: 'Descargar',
+                    type: 'downloadFile',
+                    value: pdffile
+                }],
+                title: 'Archivo PDF'
+            }
+        }
+        let imgfile = __dirname + '/resources/files/img/falabella.png'
+        let img = {
+            contentType: 'application/vnd.microsoft.card.hero',
+            content: {
+                buttons: [{
+                    title: 'Descargar',
+                    type: 'downloadFile',
+                    value: imgfile
+                }],
+                title: 'Imagen'
+            }
+        }
+        let docxfile = __dirname + '/resources/files/office/Documento_Word.docx'
+        let docx = {
+            contentType: 'application/vnd.microsoft.card.hero',
+            content: {
+                buttons: [{
+                    title: 'Descargar',
+                    type: 'downloadFile',
+                    value: docxfile
+                }],
+                title: 'Documento Word'
+            }
+        }
+        let excelfile = __dirname + '/resources/files/office/Documento_Excel.xlsx'
+        let excel = {
+            contentType: 'application/vnd.microsoft.card.hero',
+            content: {
+                buttons: [{
+                    title: 'Descargar',
+                    type: 'downloadFile',
+                    value: excelfile
+                }],
+                title: 'Archivo Excel'
+            }
+        }
+
+        await turnContext.sendActivity({
+            text: 'Archivos',
+            attachments: [pdf, img, docx, excel],
+
+        });
+
+        await LuisBot.AyudarenAlgoMas(turnContext)
+    }
+
+    static async AyudarenAlgoMas(turnContext) {
         await turnContext.sendActivities([
-            {type: 'typing'},
-            {type: 'delay', value: 1500},
-            {type: 'message', text: 'Te puedo ayudar en algo más???... 👀 '}
+            { type: 'typing' },
+            { type: 'delay', value: 1500 },
+            { type: 'message', text: 'Te puedo ayudar en algo más???... 👀 ' }
         ])
     }
 
-    static async MenuOpciones(turnContext){
+    static async MenuOpciones(turnContext) {
 
         const buttons = [
             { type: ActionTypes.ImBack, title: '1. Mostrar Video', value: 'Video' },
             { type: ActionTypes.PostBack, title: '2. Ver Reportes Anuales', value: 'Reporte' },
             { type: ActionTypes.OpenUrl, title: '3. Mostrar Página de la empresa', value: 'https://www.falabella.cl' },
             { type: ActionTypes.PostBack, title: '4. Ver Lista de Contactos ', value: 'Lista de Contacto' },
-            { type: ActionTypes.Call, title: '5. Llamar a Mesa de Ayuda', value: 'tel:123123123123' }
+            { type: ActionTypes.PostBack, title: '5. Mostrar Archivos Adjuntos ', value: 'Descargar Archivos' },
+            // { type: ActionTypes.PostBack, title: '6. Adjuntar archivo ', value: 'Adjuntar Archivo' },
+            { type: ActionTypes.Call, title: '6. Llamar a Mesa de Ayuda', value: 'tel:123123123123' }
         ];
         const card = CardFactory.heroCard('Opciones disponibles', undefined,
-        buttons, { text: 'Selecciona la opción que desees' });
+            buttons, { text: 'Selecciona la opción que desees' });
 
         // add card to Activity.
         const reply = { type: ActivityTypes.Message };
@@ -318,11 +385,11 @@ class LuisBot {
 
         // Send hero card to the user.
         await turnContext.sendActivities([
-            {type: 'delay', value: 1000},
-            {type: 'message', text: 'Esto es lo que encontré para ti... 😀 😀 😀 😀'},
-            {type: 'typing'},
-            {type: 'delay', value: 1000}
-            ]                    
+            { type: 'delay', value: 1000 },
+            { type: 'message', text: 'Esto es lo que encontré para ti... 😀 😀 😀 😀' },
+            { type: 'typing' },
+            { type: 'delay', value: 1000 }
+        ]
         );
 
         await turnContext.sendActivity(reply)
